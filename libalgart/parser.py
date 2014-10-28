b835b2a39974c042f2b9882bb3b2a849e50b1d43
Zack Marotta <zmarotta@sirfancy.com>
2014-10-28 11:45:31 -0400

    Init commit; only gray colormode working and basic arithmetic

--- libalgart/parser.py
+++ libalgart/parser.py
@@ -5,96 +5,334 @@
 # 
 #
 # Zack Marotta  (c)
-import PIL
+from PIL import Image#, ImageDraw
 import numpy as np
+import random as rnd
+import colorsys
 from collections import OrderedDict
 from user import *
+from libpyparsing.pyparsing import *  #TODO: import only what I need
 
+#The few functions below were taken from fourFn.py and SimpleCalc.py in the examples of the pyparsing lib.
+#I have commented out some of the lines and modified others. The original files can be found at the following links:
+#http://pyparsing.wikispaces.com/file/view/fourFn.py/30154950/fourFn.py
+#http://pyparsing.wikispaces.com/file/view/SimpleCalc.py/30112812/SimpleCalc.py
+
+exprStack = []
+#ParserElement.enablePackrat() #WARNING: MIGHT BREAK STUFF
+
+def pushFirst( strg, loc, toks ):
+    exprStack.append( toks[0] )
+def pushUMinus( strg, loc, toks ):
+    for t in toks:
+      if t == '-': 
+        exprStack.append( 'unary -' )
+      else:
+        break
+
+bnf = None
+def BNF():
+    """
+    expop   :: '^'
+    multop  :: '*' | '/'
+    addop   :: '+' | '-'
+    integer :: ['+' | '-'] '0'..'9'+
+    atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
+    factor  :: atom [ expop factor ]*
+    term    :: factor [ multop factor ]*
+    expr    :: term [ addop term ]*
+    """
+    global bnf
+    if not bnf:
+        point = Literal( "." )
+        #~ fnumber = Combine( Word( "+-"+nums, nums ) + 
+                           #~ Optional( point + Optional( Word( nums ) ) ) +
+                           #~ Optional( e + Word( "+-"+nums, nums ) ) )
+        #fnumber = Regex(r"[+-]?\d+(:?\.\d*)?(:?[eE][+-]?\d+)?")
+        fnumber = Regex(r"[+-]?\d+(:?\.\d*)?") #disabled sci notation because I'd rather things didn't get messy
+        #ident = Word(alphas, alphas+nums+"_$")
+        ident = Word(alphas, alphas+nums)
+        
+        plus  = Literal("+")
+        minus = Literal("-")
+        mult  = Literal("*")
+        div   = Literal("/")
+        lpar  = Literal("(").suppress()
+        rpar  = Literal(")").suppress()
+        addop  = plus | minus
+        multop = mult | div
+        expop = Literal("^")
+        
+        expr = Forward()
+        atom = ((0,None)*minus + (fnumber | ident + lpar + expr + rpar | ident).setParseAction(pushFirst) | 
+                Group(lpar + expr + rpar)).setParseAction(pushUMinus) 
+        
+        factor = Forward()                                                         #
+        factor << atom + ZeroOrMore((expop + factor).setParseAction(pushFirst))    #
+        term = factor + ZeroOrMore((multop + factor).setParseAction(pushFirst))    #
+        expr << term + ZeroOrMore((addop + term).setParseAction(pushFirst))        # Todo: Find out how the fuck this recursive stuff works
+        bnf = expr
+    return bnf
+
+arithExpr = BNF()
+ident = Word(alphas).setName("identifier")
+assignment = ident("varname") + '=' + arithExpr
+comment = Literal("#") + restOfLine
+#pattern = assignment
+pattern = assignment + Optional(comment)
+
+#comma = Literal(",").suppress()
+#arguments = delimitedList(ident("argument"))
+#function = ident + lpar + arguments + rpar 
+#fnpattern = function + '=' + arithExpr  #This was originally going to allow functions in your equations, but I wanted to keep it more simple and not slow it down. Also it didn't work.
+
+#ops = { "+" : np.add,
+#        "-" : np.subtract,
+#        "*" : np.multiply,
+#        "/" : np.divide,
+#        "^" : np.power }
+#fn  = { "sin" : np.sin,
+#        "cos" : np.cos,
+#        "tan" : np.tan,
+#        "abs" : np.absolute }
+#def evaluateStack(s):
+#    print "stack:", s
+#    op = s.pop()
+#    if op == 'unary -':
+#        return -evaluateStack(s)
+#    if op in ops:
+#        op2 = evaluateStack(s)
+#        op1 = evaluateStack(s)
+#        return ops[op](op1, op2)
+#    elif op in fn:
+#        return fn[op](evaluateStack(s))
+#    elif op[0].isalpha():
+#        raise Exception("invalid identifier '%s'" % op)
+#    else:
+#        return float(op)
+#
+#if __name__ == "__main__":
+#    import sys
+#    def test(s):
+#        global exprStack
+#        exprStack = []
+#        try:
+#            results = BNF().parseString( s, parseAll=True )
+#            val = evaluateStack( exprStack[:] )
+#        except ParseException as e:
+#            print(s, "failed parse:", str(e))
+#        except Exception as e:
+#            print(s, "failed eval:", str(e))
+#        else:
+#            print val
+#    test(sys.argv[1])
+    
 class Parser:
-    
-    
-    def __init__(self, width, height, depth, alg, verbose):
+    def __init__(self, width, height, alg, verbose, fg, bg, filename):
         self.width = width
         self.height = height
-        self.depth = depth
         self.alg = alg
         self.verbose = verbose
-        self.linedefs = OrderedDict()
-        self.constants = {"MVAL": np.exp2(depth)-1, "ROWS": height, "COLS": width, "MAX": width*height}
-        self.optvars = {"X", "Y", "P"}
-        self.locals = []
-        
-        self.ops = {"=": self._assign,
-                    "+": self._add,
-                    "-": self._subtract,
-                    "*": self._multiply,
-                    "/": self._divide,
-                    }
-        self.setReqvars()
-        
-    def setReqvars(self):
-        pass
+        self.filename = filename
+        if fg is not None:
+            self.fghue = colorsys.hsv_to_rgb(fg[0]/255, fg[1]/255, fg[2]/255)
+        else:
+            self.fghue = colorsys.hsv_to_rgb(0, 0, 1)
+        if bg is not None:
+            self.bghue = colorsys.hsv_to_rgb(bg[0]/255, bg[1]/255, bg[2]/255)
+        else:
+            self.bghue = colorsys.hsv_to_rgb(0, 0, 0)
+        self.constants = {"ROWS": height, "COLS": width, "MAX": width*height}
+        self.optvars = {"X": 0, "Y": 0, "P": 0}
+        self.locals = {}
+        self.ops = {"+" : np.add,
+                    "-" : np.subtract,
+                    "*" : np.multiply,
+                    "/" : np.divide,
+                    "^" : np.power }
+        self.fns = {"sin" : np.sin,
+                    "cos" : np.cos,
+                    "tan" : np.tan,
+                    "abs" : np.absolute }
+
+    
+    def mainSequence(self):
+        self.newImage()
+        if self.verbose: vprint("Size:{}\tPixels:{}\tCustom fg/bg color:({}, {})\nRequired variables: {}\nAlgorithm:\n{}\n".format("{}x{}".format(self.width, self.height), self.width*self.height, self.fghue, self.bghue, self.reqvars, self.alg))
+        pixeldata = self.crunch()
+        self.placePixels(pixeldata)
+        self.saveImage()
+    
+    def evaluateStack(self, s):
+        op = s.pop()
+        if op == 'unary -':             #negate
+            return -self.evaluateStack(s)
+        if op in self.ops:                   #perform op
+            op2 = self.evaluateStack(s)
+            op1 = self.evaluateStack(s)
+            return self.ops[op](op1, op2)
+        elif op in self.fns:                  #perform fn
+            return self.fns[op](evaluateStack(s))
+        elif op[0].isalpha():
+            if op in self.optvars:
+                return self.optvars[op]     #return optvar
+            if op in self.locals:
+                return self.locals[op]      #return local var
+            if op in self.constants:
+                return self.constants[op]   #return constant
+            raise Exception("invalid identifier '%s'" % op)
+        else:
+            return float(op)
         
     def crunch(self):
+        print("\n(This may take a few minutes. Please be patient!)")
+        print("Parsing...") #TODO: ADD PROGRESS BAR
+        global exprStack #not sure if this is needed
+        exprStack = []
+        pixeldata = []
+        maxp = self.constants["MAX"]
         ws = self.alg.replace("\n", ";").split(";") #lines with leading and/or trailing whitespace
         lines = map(lambda x: x.strip(), ws)
-        for linenum, l in enumerate(lines): #iterate over line_number and full line
-            if l == "": continue
-            lsplit = l.split() # ['k', '=', 'x']
-            if "=" not in lsplit:
-                if lsplit[1] != "==":
-                    missing("=", linenum, l, len(lsplit[0]))
-                else:
-                    missing("=", linenum, l, len(lsplit[0]), tip=1) #just a friendly reminder for all you fellow programmers ;)
-            if lsplit[-1:] == "=":
-                missing("expression", linenum, l, len(l)+1)
-            #for index, op in enumerate(lsplit): #iterate over index and each character
-            #    if op not in ops:
-            #        self.locals.append(op)
-            #    else:
-            #        self.ops[op](lsplit[index-1], lsplit[index+1]) #send the appropriate ops method both operands
-                
-            #print l, line, line[0], line[2:]
-            #self.linedefs[lsplit[0]] = lsplit[2:]
-    
-    
-    
-    def _assign(self):
-        pass
-    
-    def _add(self):
-        pass
-    
-    def _subtract(self):
-        pass
-    
-    def _multiply(self):
-        pass
-    
-    def _divide(self):
-        pass
-    
         
-    def setreqvars(self):
-        pass
+        for currentRow in xrange(self.height):
+            self.optvars["Y"] = currentRow
+            # 1111
+            # 2222
+            # 3333
+            for currentCol in xrange(self.width):
+                self.optvars["X"] = currentCol
+                pixval = []
+                if self.verbose: progbar(self.optvars["P"], maxp)
+                # 1234
+                # 1234
+                # 1234
+                for i, input_string in enumerate(lines):
+                    
+                    if input_string != '':
+                        del exprStack[:]
+                    
+                        isplit = input_string.split()
+                        if isplit[1] != "=":
+                            if isplit[0][0] == "$":
+                                try:
+                                    rand.seed(int(isplit[0][1:]))
+                                except:
+                                    err("ParseFailure: Line 1\n{}\n ^ invalid seed value".format(input_string))
+                            else:
+                                missing("=", i, isplit)
+                    
 
+                        #if self.verbose: vprint(input_string + "\n")#
+                        try:
+                            L=pattern.parseString(input_string, parseAll=True) #EXECUTE MAGIC.EXE    seriously this shit is insane
+                        except ParseException as pe:
+                            L=['Parse Failure',input_string]
+                      
+                      # show result of parsing the input string
+                        #if self.verbose: print(input_string, "->", L)#
+                        if len(L)==0 or L[0] != 'Parse Failure':
+                            #if self.verbose: vprint("exprStack = {}\n".format(exprStack))#
+                            try:
+                                result=self.evaluateStack(exprStack)
+                            except Exception as e:
+                                err(str(e))
+                            else:
+                                self.locals[L.varname] = result
+                            #if self.verbose: vprint("variables = {}\n".format(variables))#`
+                        else:
+                            err("ParseFailure: Line ",   more=True)
+                            err(pe.line,                 more=True)
+                            err(" "*(pe.column-1) + "^", more=True)
+                            err(pe)
+                    #end of current line
+                for v in self.reqvars:
+                    pixval.append(self.locals[v]) #[255,255,255]
+                pixeldata.append(pixval)          #[ ..., [255,255,255] ]
+                self.optvars["P"] += 1
+                #end of column
+            #end of row
+        return pixeldata
+        
+    def newImage(self):
+        pass
+    
+    def convert(p): #this will be overridden to return an RGB/CYMK tuple
+        pass
+    
+    def placePixels(self, pixeldata):
+        print("Placing pixels...")
+        maxp = self.constants["MAX"]
+        i = 0
+        for y in xrange(self.height):
+            for x in xrange(self.width):
+                if self.verbose: progbar(i, maxp)
+                color = self.convert(pixeldata[i])
+                self.pix[x,y] = color
+                i += 1
+    
+    def saveImage(self):
+        self.im.save(self.filename, "png")
+        print("Saving as {}".format(self.filename))
+        
+    
+    
+
+#http://effbot.org/imagingbook/imagedraw.htm
 
 class ParserGray(Parser):
-    def setReqvars(self):
+    def newImage(self):
         self.reqvars = ("K")
-    
-class ParserRGB(Parser):
-    def setReqvars(self):
-        self.reqvars = ("K")
+        self.im = Image.new("RGB", (self.width, self.height))
+        self.pix = self.im.load()
 
-class ParserHSL(Parser):
-    def setReqvars(self):
-        self.reqvars = ("K")
+    def convert(self, p):
+        r = lerp(self.bghue[0], self.fghue[0], p[0]/255)*255
+        g = lerp(self.bghue[1], self.fghue[1], p[0]/255)*255
+        b = lerp(self.bghue[2], self.fghue[2], p[0]/255)*255
+        return (int(round(r)), int(round(g)), int(round(b)))
 
-class ParserHSV(Parser):
-    def setReqvars(self):
-        self.reqvars = ("K")
-        
-class ParserCYMK(Parser):
-    def setReqvars(self):
-        self.reqvars = ("K")
+#class ParserRGB(Parser):
+#    def newImage(self):
+#        self.reqvars = ("R", "G", "B")
+#        self.im = Image.new("RGB", (self.width, self.height))
+#        self.pix = self.im.load()
+#    
+#    def convert(self, p):
+#        return p
+#
+#class ParserHLS(Parser):
+#    def newImage(self):
+#        self.reqvars = ("H", "L", "S")
+#        self.im = Image.new("RGB", (self.width, self.height))
+#        self.pix = self.im.load()
+#
+#    def convert(self, p):
+#        return colorsys.hls_to_rgb(p[0]/255, p[1]/255, p[2]/255)
+#    
+#class ParserHSV(Parser):
+#    def newImage(self):
+#        self.reqvars = ("H", "S", "V")
+#        self.im = Image.new("RGB", (self.width, self.height))
+#        self.pix = self.im.load()
+#
+#    def convert(self, p):
+#        return colorsys.hsv_to_rgb(p[0]/255, p[1]/255, p[2]/255)
+#
+#class ParserCYMK(Parser):
+#    def newImage(self):
+#        self.reqvars = ("C", "Y", "M", "K")
+#        self.im = Image.new("CYMK", (self.width, self.height))
+#        self.pix = self.im.load()
+#    
+#    def convert(self, p):
+#        return p
+
+#class ParserYIQ(Parser):  #TODO
+#    def newImage(self):
+#        self.reqvars = ("Y", "I", "Q")
+#        self.im = Image.new("RGB", (self.width, self.height))
+#        self.draw = ImageDraw.Draw(self.im)
+
+
+
+#Make a pull request if you want to add more modes!
--- libalgart/user.py
+++ libalgart/user.py
@@ -6,24 +6,50 @@
 #
 # Zack Marotta  (c)
 
+from __future__ import division
 import sys
 
-def err(msg, s=None, pos=None, why=None, critical=True):
+
+def err(msg, more=False):
+    """Print error in bold, bright red text. Optionally continue execution for further errors."""
     print("\x1b[91;1m" + str(msg)+ "\x1b[0m")
-    if critical:
+    if not more:
         sys.exit(1)
 
-def missing(char, linenum, line, pos, tip=None):
-    """Return error that shows where something missing is needed.
-        Format: missing(offending_character, line_number, line_string, index_of_missing_character)"""
-    if tip == None:
-        err("InvalidStatement: Missing {} on line {}\n{}\n{}".format(char, linenum + 1, line, " " * (pos+1) + "^" ))
-    elif tip == 1:
-        err("InvalidStatement: Missing {} on line {}\n{}\n{}\n(Did you mean \"=\"? ;-)".format(char, linenum + 1, line, " " * (pos+1) + "^" ))
+def vprint(msg):
+    """Print a message in bold, bright yellow text for debugging in verbose mode."""
+    sys.stderr.write("\x1b[93m" + str(msg) + "\x1b[0m")
+
+
+def missing(char, linenum, line): #was used for any missing character, but that's now handled by pyparsing,
+    tip = False                   #> so now it's adapted for missing "="s in assignments
+    if line[1] == "==":
+        tip = True
+    lstr = line.join(" ")
+    pos = len(line)[0] + 1
     
-def isNum(x):
+    err("InvalidStatement: Missing '{}' on line {}\n{}\n{} expected '='".format(char, linenum + 1, lstr, " " * pos + "^" ), more=tip)
+    if tip:
+        err("\n(Did you mean \"=\"? ;-)") #we've all done it :)
+    sys.exit(1)
+    
+def isNum(x): #legacy; probably won't be used anymore.    ...heh. It's funny I'm calling something legacy even before the first release.
+    """Check if x is a number."""
     try:
         float(x)
         return True
     except ValueError:
         return False
+    
+def lerp(v0, v1, p):
+    """Linear interpolation between tuples v0 and v1 with phase p"""
+    return (1 - p) * v0 + p * v1
+
+def progbar(p, f):
+    """Display a progress bar between 0 and f with progress p in verbose"""
+    i = int(round(p/f * 60))
+    verbose("\x1b[93m[{}{}] {} / {}\x1b[0m\r".format("="*i, "-"*(60-i), p, f))
+    if p == f:
+        print()
+    
+    
